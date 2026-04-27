f311e3622d76d42d8b0afd30303bd6821628bbd5
Давид <david.sebelev@gmail.com>
2026-04-27 23:27:54 +0500

    Implement Researcher interface

--- university_system/src/model/research/Researcher.java
+++ university_system/src/model/research/Researcher.java
@@ -1,5 +1,17 @@
 package model.research;
 
-public interface Researcher {
+import java.util.Comparator;
+import java.util.List;
 
+public interface Researcher {
+    int calculateHIndex(); // h index , h states = named h times
+
+    void printPapers(Comparator<ResearchPaper> comparator);
+
+    List<ResearchPaper> getResearchPapers();
+    List<ResearchProject> getResearchProjects();
+
+    void addPaper(ResearchPaper paper);
+
+    void addProject(ResearchProject project);
 }
