a3b78a1bcda57ef6ac53ae6a8c03257dbdfc1ec3
user <example@mail.com>
2026-04-28 00:06:28 +0500

    adding academic models draft

--- university_system/src/model/academic/Lesson.java
+++ university_system/src/model/academic/Lesson.java
@@ -1,5 +1,37 @@
 package model.academic;
 
-public class Lesson {
+import java.time.LocalDateTime;
 
+import enums.LessonType;
+import model.users.Teacher;
+
+public class Lesson {
+	private Teacher teacher;
+	private Course course;
+	private String room;
+	private LocalDateTime schedule;
+	private LessonType lessonType;
+	
+	public Lesson(Teacher teacher, Course course, String room, LocalDateTime schedule, LessonType lessonType) {
+	    this.teacher = teacher;
+	    this.course = course;
+	    this.room = room;
+	    this.schedule = schedule;
+	    this.lessonType = lessonType;
+	}
+	
+	public Teacher getTeacher() { return teacher; }
+	public Course getCourse() { return course; }
+	public LocalDateTime getSchedule() { return schedule; }
+	public LessonType getLessonType() { return lessonType; }
+	
+	@Override 
+	public String toString() {
+		return "\nLesson:" +
+		       "\nCourse: " + course +
+		       "\nTeacher: " + teacher +
+		       "\nType: " + lessonType +
+		       "\nRoom: " + room +
+		       "\nTime: " + schedule + "\n";
+	}
 }
--- university_system/src/model/academic/Transcript.java
+++ university_system/src/model/academic/Transcript.java
@@ -1,5 +1,42 @@
 package model.academic;
 
-public class Transcript {
+import java.util.*;
 
+import model.users.Student;
+
+public class Transcript {
+	private Student student;
+	private List<Mark> marks = new ArrayList<>();
+	private double gpa;
+	
+	public Transcript(Student student) {
+	    this.student = student;
+	    this.marks = new ArrayList<>();
+	}
+	
+	public void generate(List<Enrollment> enrollments) {}
+	
+	 private double convert(String g) {
+	        switch (g) {
+	            case "A": return 4.0;
+	            case "A-": return 3.7;
+	            case "B+": return 3.3;
+	            case "B": return 3.0;
+	            case "B-": return 2.7;
+	            case "C+": return 2.3;
+	            case "C": return 2.0;
+	            case "C-": return 1.7;
+	            case "D+": return 1.3;
+	            case "D": return 1.0;
+	            default: return 0;
+	        }
+	    }
+	
+	public void print() {
+		System.out.println(this);
+	}
+	@Override
+	public String toString() {
+		 return "Transcript: " + student + " GPA: " + gpa;
+	}
 }
